f183915988f047ab6e8d2007bb69d67e3605f2b3
matthew7402 <105174746+matthew7402@users.noreply.github.com>
2022-05-09 00:11:15 +0200

    Add files via upload
    Atmana el class y3gbkom

---
+++ Cinema.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Sherief.Shalaby
+ */
+public class Cinema {
+    String cinemaName;
+    int noOfSeats;
+    double price;
+    String location;
+    double openTime;
+    double closeTime;
+    Movie [] movies;
+    Menu [] menu;
+
+    public Cinema(String cinemaName, int noOfSeats, double price, String location, double openTime, double closeTime, Movie[] movies, Menu[] menu) {
+        this.cinemaName = cinemaName;
+        this.noOfSeats = noOfSeats;
+        this.price = price;
+        this.location = location;
+        this.openTime = openTime;
+        this.closeTime = closeTime;
+        this.movies = movies;
+        this.menu = menu;
+    }
+
+    public void setCinemaName(String cinemaName) {
+        this.cinemaName = cinemaName;
+    }
+
+    public void setNoOfSeats(int noOfSeats) {
+        this.noOfSeats = noOfSeats;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setOpenTime(double openTime) {
+        this.openTime = openTime;
+    }
+
+    public void setCloseTime(double closeTime) {
+        this.closeTime = closeTime;
+    }
+
+    public void setMovies(Movie[] movies) {
+        this.movies = movies;
+    }
+
+    public void setMenus(Menu[] menus) {
+        this.menu = menus;
+    }
+    public void viewInfo(){
+        System.out.println("Cinemaname:"+cinemaName); 
+        System.out.println("price:"+price);
+        System.out.println("opening time:"+openTime);
+        System.out.println("opening time:"+closeTime);
+        System.out.println("location:"+location);
+        System.out.println("movies available");
+        for (int index=0;index<movies.length;index++) {
+            System.out.print(movies[index]);
+        }
+    }
+
+
+    
+}
