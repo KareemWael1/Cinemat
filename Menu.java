e9c8d2cb7da1c4104b4d98342926a23fb87cc0ea
FadyFady01 <105220427+FadyFady01@users.noreply.github.com>
2022-05-09 15:37:06 +0200

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,40 @@
+package com.mycompany.softwareengproject;
+
+public class Menu {
+    String food;
+    String drinks;
+    float foodPrice;
+    float drinksPrice;
+    
+    public Menu() {
+    }
+
+    public Menu(String food, String drinks, float foodPrice, float drinksPrice) {
+        this.food = food;
+        this.drinks = drinks;
+        this.foodPrice = foodPrice;
+        this.drinksPrice = drinksPrice;
+    }
+
+    public Menu(String food, float foodPrice) {
+        this.food = food;
+        this.foodPrice = foodPrice;
+    }
+
+    public void setFoodPrice(float foodPrice) {
+        this.foodPrice = foodPrice;
+    }
+
+    public float getFoodPrice() {
+        return foodPrice;
+    }
+
+    public void setDrinksPrice(float drinksPrice) {
+        this.drinksPrice = drinksPrice;
+    }
+
+    public float getDrinksPrice() {
+        return drinksPrice;
+    }
+    
+}
