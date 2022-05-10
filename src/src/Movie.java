b3a22f103f846d928bed66f1db7aff9b59e19d99
Ahmed Tarek <7madak10@gmail.com>
2022-05-10 22:11:20 +0200

    added methods in movie class

--- src/src/Movie.java
+++ src/src/Movie.java
@@ -1,14 +1,34 @@
 public class Movie {
     private final String title;
+    private final int duration;
     private int availableTickets=60;
     private float rating=0;
+    private int numOfReviews;
 
-    Movie(String title){
+    Movie(String title,int duration){
         this.title = title;
+        this.duration=duration;
     }
+
+
     public boolean availableTickets(int numberOfTickets){
         return availableTickets>numberOfTickets;
     }
+    public void bookTickets(int numOfTickets){
+        if (availableTickets(numOfTickets)){
+            this.availableTickets -= numOfTickets;
+            System.out.println("Book confirmed");
+        }
+        else {
+            System.out.println("there is error");
+        }
+    }
+    public void addRate(float rate){
+        numOfReviews++;
+        rating = rate/numOfReviews;
+    }
 
-
+    public int getDuration() {
+        return duration;
+    }
 }
